67c1fa486e4531bff335e8c9768742e7d894659e
Frank <Frank@hotmail.com>
2018-01-19 11:06:37 +0800

    Initial commit

---
+++ app/src/main/java/com/frandog/a20180119_01/MainActivity.java
@@ -0,0 +1,17 @@
+package com.frandog.a20180119_01;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
